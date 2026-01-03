dabc755c6a987091dc12f7670c4ee0b760c2a4fe
JRobber66 <141540195+JRobber66@users.noreply.github.com>
2026-01-03 15:04:34 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/refnull/",   // 👈 REQUIRED for GitHub Pages
-});
+  base: '/Tester/', // ✅ must match GitHub Pages branch/repo path
+})
